85f34d6a11508c663adff56b1a0d6b23f56b7dd6
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-05-04 13:25:30 +0200

    no shuffling answers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
   <Card>
     <p slot="title">{currentQuestion.question}</p>
     <div class="flex flex-col gap-4">
-      {#each shuffle(currentQuestion.answers) as { answer, correct }}
+      {#each currentQuestion.answers as { answer, correct }}
         <Button green={correct && isAnswered} on:click={() => (isAnswered = true)}>{answer}</Button>
       {/each}
 
